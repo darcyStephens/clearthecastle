8192810ce94e2a14da5736ade44ad2da342eaa42
darcyStephens <darcystephens40@gmail.com>
2023-08-02 19:30:46 +0930

    prac2

---
+++ function-2-1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+extern void print_binary_str(std::string decimal_number)
+{
+    int divisor, dividend, remainder[20];
+    int i = 0, counter = 0;
+
+    int int_num = stoi(decimal_number);
+    divisor = 2;
+
+    while (int_num >= 1)
+    {
+
+        std::cout << " Quotient =  " << int_num << std::endl;
+
+        remainder[i] = int_num % divisor;
+        int_num = int_num / divisor;
+
+        i++;
+        counter++;
+    }
+    for (int j = counter; j--; j > 0)
+    {
+        std::cout << remainder[j];
+    }
+    std::cout << "\n"
+              << std::endl;
+
+
+}
