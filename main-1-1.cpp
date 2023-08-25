0f4bae9ea3de6647093bcc947931aa26f7135275
darcyStephens <darcystephens40@gmail.com>
2023-08-25 15:03:30 +0930

    fucksake

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -5,14 +5,12 @@
 int main() {
 
 Bike b1; //creating instance with default constructor
+b1.get_brand();
+b1.get_code();
 
-std::cout << "Default brand: " << b1.get_brand() << ", Default code: " << b1.get_code() << std::endl; // calling functions of instance made with default constructor
-
-Bike b2("super", 56); // creating instance with constructor with members
-
-std::cout << "Brand: " << b2.get_brand() << ", Code: " << b2.get_code() << std::endl;// calling get functions to display instrument
-
-
+Bike b2("brand", 1);
+b2.get_brand();
+b2.get_code();
 
 return 0;
 
